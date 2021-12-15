fae52c0e1046f7cdd3ea722673266cf5d0a9e722
Parag Gawde <paraggawde1@gmail.com>
2021-12-15 15:41:34 +0530

    first commit

---
+++ src/main/java/com/example/demoprojectTwo/DemoprojectTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoprojectTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojectTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoprojectTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoprojectTwo/DemoprojectTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoprojectTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojectTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
